2aee0e39a0667198efd6931c94b7ed9fb76a3bda
Harrison Grant <Harry.grant99@googlemail.com>
2022-10-04 13:56:56 +0100

    setup initial values, while mapping through bakery & cafe lists. Totalled up orderCals & orderPrice (not including promotions yet)

--- programming-exercise/script.js
+++ programming-exercise/script.js
@@ -86,50 +86,43 @@ const promotions = [
 ];
 
 const calculateBasket = (products) => {
-  // JavaScript exercise
-  // This function mysteriously got wiped from our online ordering system, and there's no history of it in source control...
-  // The function takes an array of product names (see the calls below).
-  // It needs to calculate the order total both in terms of price and of calories and return them in an object:
-  // {
-  //     totalPrice: ???,
-  //     totalCalories: ???
-  // }
-  // The bakeryProducts and cafeProducts arrays contain all the details you'll need about the business' products (pretend the data is from a database or REST API).
-  //
-  // For extra points, check if any promotions (from the promotions array) can be applied, based on the products that are passed in, and reflect any change in total price.
-  // Promotions should override the price of a pair of products,
-  // where one of the products is found in the "applicableFromBakery" array and the other is found in the "applicableFromCafe" array.
-  // Example:
-  // products = ["toast", "tea", "blueberry muffin"]
-  // totalPrice would normally be 1.0 (toast) + 1.0 (tea) + 2.5 (muffin) = 4.5
-  // With the "breakfast to go" promotion, we can override the cost of the tea and toast with 1.8:
-  // totalPrice becomes 1.8 (toast + tea promotion) + 2.5 (muffin) = 4.3
-  //
-  // A basic implementation could search for valid pairs of products and apply promotions in the order that it finds them
-  // A more advanced version could calculate the ideal promotions to apply, resulting in the lowest possible total
-  //
-  // Do as little or as much as you feel like of this exercise, the point isn't to pass/fail your JavaScript skills,
-  // but to see how you go about using the language and how you approach solving a given problem
-};
 
-// Exercise output (don't change anything of these but feel free to add more tests)
-console.log(calculateBasket(["brownie", "iced coffee"]));
+  // setting up our inital values
+  let orderCals = 0;
+  let orderPrice = 0;
 
-console.log(calculateBasket(["blueberry muffin", "carrot cake"]));
+  /******** bakeryProducts Total********/
+  
+    const bakeryArr = bakeryProducts.map(product => product.name)
 
-console.log(calculateBasket(["savoury muffin", "coffee", "hot chocolate"]));
+    // if our (products) name == the (bakeryArr) name, it will add that 
+    // corresponding calories & unit price
+    for (let i = 0; i < products.length; i++) {
+      for (let j = 0; j < bakeryArr.length; j++) {
+        if (products[i] == bakeryArr[j]) {
+          orderCals += bakeryProducts[j].calories;
+          orderPrice += bakeryProducts[j].unitPrice;
+        }
+      }
+    }
 
-console.log(
-  calculateBasket([
-    "brownie",
-    "carrot cake",
-    "sandwich",
-    "coffee",
-    "coffee",
-    "iced coffee",
-  ])
-);
+    /******** cafeProducts Total********/
+    
+    const cafeArr = cafeProducts.map(product => product.name)
 
-console.log(
-  calculateBasket(["sponge", "tea", "savoury muffin", "coffee", "sponge"])
-);
+
+    // same as above, going through cafeProducts
+    for (let i = 0; i < products.length; i++) {
+      for (let j = 0; j < cafeArr.length; j++) {
+        if (products[i] == cafeArr[j]) {
+          orderCals += cafeProducts[j].calories;
+          orderPrice += cafeProducts[j].unitPrice;
+        }
+      }
+    }
+    console.log(orderPrice)
+    // CURRENT TOTALS
+    // e.g. ["savoury muffin", "coffee", "hot chocolate"] 
+    // orderCals = 190
+    // orderPrice = 5
+  }
